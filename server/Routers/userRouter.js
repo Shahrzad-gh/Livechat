d3e086d494165839935db817fd25f3965566317b
Shahr Zad <gh.shahrzad@gmail.com>
2021-09-28 19:13:16 +0330

    update user route - add auth / currentUser / getUser

--- server/Routers/userRouter.js
+++ server/Routers/userRouter.js
@@ -4,6 +4,9 @@ const router = Router();
 
 router.post("/register", userController.signup);
 router.post("/login", userController.signin);
-router.get("/signout", userController.signout);
+//router.get("/signout", userController.signout);
+router.get("/auth", userController.loggedIn);
+router.get("/currentuser", userController.getCurrentUser);
+router.get("/getuser", userController.getUser);
 
 module.exports = router;
