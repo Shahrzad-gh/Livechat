0931c9edc84767ebff9377702ac040765cb0804c
Shahr Zad <gh.shahrzad@gmail.com>
2021-09-28 19:12:08 +0330

    update user Model

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -2,32 +2,74 @@ const mongoose = require("mongoose");
 const { isEmail } = require("validator");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      //require: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      validate: [isEmail, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [8, "Minimum password length is 8 characters"],
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true,
-    validate: [isEmail, "Please enter a valid email"],
-  },
-  password: {
-    type: String,
-    required: [true, "Please enter a password"],
-    minlength: [8, "Minimum password length is 8 characters"],
-  },
-  avatar: {
-    type: String,
-    required: false,
-  },
-});
+  { timestamps: true }
+);
 
 // fire a function before doc saved to db
 userSchema.pre("save", async function (next) {
