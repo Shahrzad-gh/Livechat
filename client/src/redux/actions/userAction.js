5df7702a515a9419d155ce84ae8f7ab3a2370539
Shahr Zad <gh.shahrzad@gmail.com>
2021-09-28 19:14:51 +0330

    update user action - add id

--- client/src/redux/actions/userAction.js
+++ client/src/redux/actions/userAction.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { userConstant } from "./Types";
 
-export const getUser = () => {
+export const getUser = (id) => {
   return async (dispatch) => {
     let res;
     try {
-      res = await axios.get("/getuser");
+      res = await axios.get("/getuser", id);
       dispatch({
         type: userConstant.GET_USER_SUCCESS,
         payload: res.data,
